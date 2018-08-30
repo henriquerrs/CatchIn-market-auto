1785fc97d13f8d15cff394ea13d69272cc6b945f
Leonardo Airam Vieira <leonardo.airam@outlook.com>
2018-08-30 08:45:25 -0300

    Adicionada telas de login e cadastro

---
+++ CatchInTeste/src/java/br/com/entra21java/bean/PrivilegioBean.java
@@ -0,0 +1,29 @@
+package br.com.entra21java.bean;
+
+/**
+ * @author Alunos
+ */
+public class PrivilegioBean {
+    
+    private int id;
+    private String tipo;
+  
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    
+    
+}
