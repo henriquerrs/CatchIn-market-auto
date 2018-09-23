080d2881c291328e4c84972083f50aa3a18860d4
H3nr1 <henrique.rrs@hotmail.com>
2018-09-23 06:14:06 -0300

    excluindo item no admin

--- CatchInTeste/src/java/br/com/entra21java/web/supermecado/SupermercadoStoreExcluirProduto.java
+++ CatchInTeste/src/java/br/com/entra21java/web/supermecado/SupermercadoStoreExcluirProduto.java
@@ -6,6 +6,7 @@
 package br.com.entra21java.web.supermecado;
 
 import br.com.entra21java.dao.ItemDAO;
+import br.com.entra21java.dao.ProdutoDAO;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,10 +24,12 @@ public class SupermercadoStoreExcluirProduto extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         resp.setContentType("text/html; charset=UTF-8");
+         
+        int id = Integer.parseInt(req.getParameter("id"));
         
-        int idItem = Integer.parseInt(req.getParameter("id"));
-        
-        if(new ItemDAO().excluirItem(idItem))resp.sendRedirect("");
+         new ProdutoDAO().excluirProduto(id);
+         resp.sendRedirect("");
     }
     
 }
